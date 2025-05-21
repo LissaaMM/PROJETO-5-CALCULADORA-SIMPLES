ad81969212090d24d44b9750a8b4157166f170c9
Ana Larissa Mendes <152804250+LissaaMM@users.noreply.github.com>
2025-05-21 19:43:18 +0000

    hurum

--- src/components/Calculadora.jsx
+++ src/components/Calculadora.jsx
@@ -1,11 +1,33 @@
 import React from 'react'
 import '../components/calculadora.css'
 
+export default function Calculadora(){
+  const [num1, setNum1]= useState('')
+  const [num2, setNum2]= useState('')
+  const [resultado, setResultado]= useState('')
+}
+function somar(){
+  setResultado(Number(num1)+Number(num2))
+}
+function subtrair(){
+  setResultado(Number(num1)-Number(num2))
+}
+function multiplicar(){
+  setResultado(Number(num1)*Number(num2))
+}
+function dividir (){
+  setResultado(Number(num1)/Number(num2))
+}
 const Calculadora = () => {
   return (
     <div className="comeco">
       <h1>Calculadora Simples</h1>
-      <input type="number" placeholder="Digite o primeiro número" />
+
+      <input type="number" 
+      placeholder="Digite o primeiro número" 
+      value={num2} 
+      onChangeCapture={(e) =>setNum1}
+      />
       <br />
       <input type="number" placeholder="Digite o segundo número" />
       <br />
